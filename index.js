b599ef1246abdb23d5ec3a258c264a85f1c030ad
ivanaevans <evans.carolina@gmail.com>
2020-07-03 10:52:46 -0300

    cambios

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const app = 1;
 const main = 5;
 const a = 3;
-const b = 5;
-const c = 10;
+const b = 5555;
+const c = 105;
 const x = 10;
 
 const likes = 3;
+
+const ivana = 14;
+const pelusa = 155;
